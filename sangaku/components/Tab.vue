451077d1dde496e3e8a3bc2d5e3e170418602a10
tsunpoko <exploit.py@gmail.com>
2020-01-23 01:54:06 +0900

    Tabbar is implemented

---
+++ sangaku/components/Tab.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item has-text-weight-semibold is-family-sans-serif" href="/">
+          <i class="fab fa-battle-net"></i>
+          <p class="mdi mdi-light">
+            &nbsp; 沖縄工業高等専門学校 産学連携協力会 &nbsp;
+          </p>
+          <i class="fab fa-battle-net"></i>
+        </a>
+
+        <a role="button" class="navbar-burger burger " aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start title-tabbar">
+          <a class="navbar-item" :class="getIsActive('/')" href="/">
+            トップ
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/kaicho')" href="/kaicho">
+            会長挨拶
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/kaisoku')" href="/kaisoku">
+            会則
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/yakuin')" href="/yakuin">
+            役員
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/kaiin')" href="kaiin">
+            会員
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/houkoku')" href="/houkoku">
+            事業報告
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/keikaku')" href="/keikaku">
+            事業計画
+          </a>
+
+          <a class="navbar-item" :class="getIsActive('/form')" href="/form">
+            各種お手続き
+          </a>
+
+          <a class="navbar-item" target="_blank" href="http://www.okinawa-ct.ac.jp/">
+            沖縄高専HPへ
+          </a>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    getIsActive(path) {
+      console.log("path :", this.$route.path)
+      return path == this.$route.path ? "is-active" : ""
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.title-tabbar {
+  font-size: 16px;
+  font-weight: 600;
+}
+
+</style>
--- sangaku/layouts/default.vue
+++ sangaku/layouts/default.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <nuxt />
+    <tab />
   </div>
 </template>
 
+<script>
+import Tab from '~/components/Tab.vue'
+
+export default {
+  components: {
+    Tab
+  }
+}
+</script>
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -52,4 +61,5 @@ html {
   color: #fff;
   background-color: #35495e;
 }
+
 </style>
